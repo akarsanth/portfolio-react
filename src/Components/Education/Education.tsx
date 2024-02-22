1153a6e65be7daf9f23e8b92fc2f099f7932f19a
Aakarshan Thapa <akarsan.thapa@gmail.com>
2024-02-22 11:37:07 -0500

    Update education section

--- src/Components/Education/Education.tsx
+++ src/Components/Education/Education.tsx
@@ -6,7 +6,7 @@ type ActiveState = "education" | "experience";
 
 // Education Section
 const Education = () => {
-  const [active, setActive] = useState<ActiveState>("experience");
+  const [active, setActive] = useState<ActiveState>("education");
 
   return (
     <AnimatedSection css={styles.tab__education}>
@@ -32,7 +32,45 @@ const Education = () => {
         {/* <hr className="separator" /> */}
       </div>
 
-      <div className={styles.right}></div>
+      <div className={styles.right}>
+        {active === "education" && (
+          <div className={styles.education__list}>
+            <div className={styles["education__list-item"]}>
+              <p className={styles.time}>Jan 2024 &#8213; Present</p>
+              <div>
+                <p className={styles.institution}>
+                  Lambton College Mississauga
+                </p>
+                <p className={styles.course}>Full Stack Software Development</p>
+
+                <p className={styles.description}>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Aperiam illum autem aspernatur odio delectus, similique
+                  accusantium, natus hic ad a dignissimos, minus ipsum minima!
+                </p>
+              </div>
+            </div>
+
+            <div className={styles["education__list-item"]}>
+              <p className={styles.time}>2019 &#8213; 2022</p>
+              <div>
+                <p className={styles.institution}>
+                  London Metropolitan University
+                </p>
+
+                <p className={styles.course}>BSc(Hons) Computing</p>
+
+                <p className={styles.description}>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Aperiam illum autem aspernatur odio delectus, similique
+                  accusantium, natus hic ad a dignissimos, minus ipsum minima!
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+        {active === "experience" && <div className="experience__list"></div>}
+      </div>
     </AnimatedSection>
   );
 };
