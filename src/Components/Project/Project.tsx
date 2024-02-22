24afb2484777b63dc1f37008fbd0ab0ee97e12a4
Aakarshan Thapa <akarsan.thapa@gmail.com>
2024-02-22 13:06:20 -0500

    Change about links flex direction

--- src/Components/Project/Project.tsx
+++ src/Components/Project/Project.tsx
@@ -2,7 +2,12 @@ import AnimatedSection from "../AnimatedSection";
 import "./Project.css";
 
 const Project = () => {
-  return <AnimatedSection css="tab__project">Project</AnimatedSection>;
+  return (
+    <AnimatedSection css="tab__project">
+      <h1>Under Construction</h1>
+      <p>I am currenly updating this portfolio website.</p>
+    </AnimatedSection>
+  );
 };
 
 export default Project;
