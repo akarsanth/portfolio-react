cee405bc7cc714c2229969e0deac572383a5a4be
Aakarshan Thapa <akarsan.thapa@gmail.com>
2024-02-23 00:33:08 -0500

    Add links to education and experience items

--- src/Components/Education/Education.tsx
+++ src/Components/Education/Education.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AnimatedSection from "../AnimatedSection";
 import styles from "./Education.module.css";
+import { MdArrowOutward } from "react-icons/md";
 
 type ActiveState = "education" | "experience";
 
@@ -33,15 +34,24 @@ const Education = () => {
       </div>
 
       <div className={styles.right}>
+        {/* Education */}
         {active === "education" && (
           <div className={styles.education__list}>
-            <div className={styles["education__list-item"]}>
+            <a
+              className={styles["education__list-item"]}
+              href="https://www.lambtoncollege.ca/programs/international/fsds"
+              target="_blank"
+            >
               <p className={styles.time}>Jan 2024 &#8213; Present</p>
+
               <div>
                 <p className={styles.institution}>
                   Lambton College Mississauga
+                  <span>
+                    <MdArrowOutward />
+                  </span>
                 </p>
-                <p className={styles.course}>Full Stack Software Development</p>
+                <p className={styles.title}>Full Stack Software Development</p>
 
                 <p className={styles.description}>
                   Actively pursuing Full Stack Software Development with a focus
@@ -50,17 +60,24 @@ const Education = () => {
                   creating user-centric web applications.
                 </p>
               </div>
-            </div>
+            </a>
 
-            <div className={styles["education__list-item"]}>
+            <a
+              className={styles["education__list-item"]}
+              href="https://www.londonmet.ac.uk/courses/undergraduate/computing---bsc-hons/"
+              target="_blank"
+            >
               <p className={styles.time}>2019 &#8213; 2022</p>
 
               <div>
                 <p className={styles.institution}>
                   London Metropolitan University
+                  <span>
+                    <MdArrowOutward />
+                  </span>
                 </p>
 
-                <p className={styles.course}>BSc(Hons) Computing</p>
+                <p className={styles.title}>BSc(Hons) Computing</p>
 
                 <p className={styles.description}>
                   Acquired essential skills in commercial applications and
@@ -69,10 +86,40 @@ const Education = () => {
                   for diverse roles in the field.
                 </p>
               </div>
-            </div>
+            </a>
           </div>
         )}
-        {active === "experience" && <div className="experience__list"></div>}
+
+        {/* Experience */}
+        {active === "experience" && (
+          <div className={styles.experience__list}>
+            <a
+              className={styles["experience__list-item"]}
+              href="https://www.ekbana.com/"
+              target="_blank"
+            >
+              <p className={styles.time}>Aug 2022 &#8213; Nov 2022</p>
+              <div>
+                <p className={styles.institution}>
+                  E.K. Solutions Pvt. Ltd.
+                  <span>
+                    <MdArrowOutward />
+                  </span>
+                </p>
+                <p className={styles.title}>
+                  Frontend Web Developer Internship
+                </p>
+
+                <p className={styles.description}>
+                  Acquired proficiency in React Frontend development, utilizing
+                  tools/libraries like Formik, Redux, React Router, and Next JS.
+                  Gained essential knowledge in Git/Github, QA, SDLC, API, and
+                  Postman to enhance software development skills.
+                </p>
+              </div>
+            </a>
+          </div>
+        )}
       </div>
     </AnimatedSection>
   );
