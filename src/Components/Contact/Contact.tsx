12bb020a723cb4ee883c36506bef41b926c47305
Aakarshan Thapa <akarsan.thapa@gmail.com>
2024-02-24 22:24:01 -0500

    Increase accessibility of contact section

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -11,23 +11,57 @@ const Contact = () => {
   return (
     <AnimatedSection css="tab__contact">
       <div className="links-wrapper">
-        <a href="https://www.linkedin.com/in/thapaakarsan" target="_blank">
+        {/* Linkedin */}
+        <a
+          href="https://www.linkedin.com/in/thapaakarsan"
+          target="_blank"
+          aria-label="LinkedIn Profile Link"
+          rel="noopener noreferrer"
+        >
           <FaLinkedinIn className="link-icon" />
           <span className="pb-1">linkedin.com/in/thapaakarsan</span>
         </a>
-        <a href="https://github.com/akarsanth" target="_blank">
+
+        {/* Github */}
+        <a
+          href="https://github.com/akarsanth"
+          target="_blank"
+          aria-label="GitHub Profile Link: akarsanth"
+          rel="noopener noreferrer"
+        >
           <FaGithub className="link-icon" />
           <span>github.com/akarsanth</span>
         </a>
-        <a href="https://www.instagram.com/akarsanthapa" target="_blank">
+
+        {/* Instagram */}
+        <a
+          href="https://www.instagram.com/akarsanthapa"
+          target="_blank"
+          aria-label="Instagram Profile Link: akarsanthapa"
+          rel="noopener noreferrer"
+        >
           <FaInstagram className="link-icon" />
           <span className="pb-1">instagram.com/akarsanthapa</span>
         </a>
-        <a href="https://www.x.com/ThapaAkarsan" target="_blank">
+
+        {/* Twitter */}
+        <a
+          href="https://www.x.com/ThapaAkarsan"
+          target="_blank"
+          aria-label="Twitter Profile Link: ThapaAkarsan"
+          rel="noopener noreferrer"
+        >
           <FaXTwitter className="link-icon" />
           <span>x.com/ThapaAkarsan</span>
         </a>
-        <a href="mailto:akarsan.thapa@gmail.com" target="_blank">
+
+        {/* Mail */}
+        <a
+          href="mailto:akarsan.thapa@gmail.com"
+          target="_blank"
+          aria-label="Send an email to akarsan.thapa@gmail.com"
+          rel="noopener noreferrer"
+        >
           <FiMail className="link-icon" />
           <span className="pb-2">akarsan.thapa@gmail.com</span>
         </a>
